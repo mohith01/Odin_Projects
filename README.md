bad4da234341b024b2bc2df54f3d664b881fb257
mohith01 <mohith01@gmail.com>
2024-02-29 22:13:05 -0500

    Create UI for rock-paper-scissors

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## ODIN Recipes
 
 A Recipe blog that is a refresher for my frontend skills and to force myself to work on the ODIN Project
+
+
+YCombinator - Continue
--- javascript-ex/rock-paper-scissors-v1/game.js
+++ javascript-ex/rock-paper-scissors-v1/game.js
@@ -1,28 +1,69 @@
 console.log("Hello World");
 
-function game() {
-  let choice = prompt("Please enter your choice: rock, paper, scissors")
-  let arr = ["rock", "paper", "scissors"];
+let arr = ["rock", "paper", "scissors"];
+
+function game(choice) {
+  // let choice = prompt("Please enter your choice: rock, paper, scissors")
+  // let arr = ["rock", "paper", "scissors"];
   let pc_choice = arr[Math.floor(Math.random() * arr.length)];
   let user = choice.trim().toLowerCase();
   if (user == pc_choice) {
-    console.log("try_again")
-    game();
+    // console.log("try_again")
+    return ["Both played the same", 0, 0]
   }
   else if (((user == "rock") && (pc_choice == "scissors")) || ((user == "paper") && (pc_choice == "rock")) || ((user == "scissors") && (pc_choice == "paper"))) {
-    console.log(`The computer choice is ${pc_choice}. User ${user} has won the game`);
-    return
+    // console.log(`The computer choice is ${pc_choice}. User ${user} has won the game`);
+    return [`The computer choice is ${pc_choice}. User ${user} has won the game` , 0 ,1]
   }
   else if ((!arr.includes(user))) {
-    console.log("Invalid Input");
-    game();
+    // console.log("Invalid Input");
+    return [`Invalid Input`, 0, 0]
   }
   else {
-    console.log(`The computer choice is ${pc_choice}. User ${user} has lost the game`);
-    return;
+    // console.log(`The computer choice is ${pc_choice}. User ${user} has lost the game`);
+    return [`The computer choice is ${pc_choice}. User ${user} has lost the game` ,1 , 0]
   }
-
 
 }
 
-game()
+const container = document.querySelector('#container');
+const feedback = document.createElement('h2');
+
+feedback.textContent = "Game"
+container.appendChild(feedback);
+var userScore = 0
+var pcScore = 0
+arr.forEach((element, index) => {
+var buttons = document.createElement('button');
+buttons.textContent= element;
+buttons.style.color = "Blue";
+buttons.style.padding = "25px 25px";
+buttons.style.margin = "20px"
+buttons.addEventListener('click', function(e) {
+  if (userScore>=5 || pcScore>=5) {
+    return;
+  }
+  var [returnFeedback, pcWin, userWin] = game(element);
+  console.log(returnFeedback,pcWin,userWin);
+  feedback.textContent = returnFeedback;
+  var tempElem = document.createElement('h4')
+
+  userScore+=userWin
+  pcScore+=pcWin
+
+  tempElem.textContent = `User Score: ${userScore} Pc Score: ${pcScore} User Element: ${element}`
+  container.appendChild(tempElem)
+
+  if (userScore==5) {
+    feedback.textContent = "Congrats, User has won the game"
+  }
+  else if (pcScore==5){
+    feedback.textContent = "Congrats, PC has won the game"
+  }
+
+
+});
+container.appendChild(buttons)
+});
+
+// game()
